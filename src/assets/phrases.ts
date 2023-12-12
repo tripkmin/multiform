98482cd123dab73032077cf27866160cb8a27478
tripkmin <tripkmin@gmail.com>
2023-12-12 18:26:34 +0900

    feat: InitialIze project setup

---
+++ src/assets/phrases.ts
@@ -0,0 +1,29 @@
+export const phrases = {
+  step1: {
+    head: 'Personal Info',
+    subHead: 'Please provide your name, email address, and phone number.',
+    namePlaceHolder: 'e.g. Stephan King',
+    emailPlaceHolder: 'e.g. stephanking@lorem.com',
+    phonePlaceHolder: 'e.g. +1 234 567 890',
+  },
+  step2: {
+    head: 'Select your plan',
+    subHead: 'You have the option of monthly or yearly billing.',
+  },
+  step3: {
+    head: 'Pick add-ons',
+    subHead: 'Add-ons help enhance your gaming experience.',
+    onlineService: 'Access to multiplayer games',
+    largerStorage: 'Extra 1TB of cloud save',
+    CustomizableProfile: 'Custom theme on your profile',
+  },
+  step4: {
+    head: 'Finishing up',
+    subHead: 'Double-check everything looks OK before confirming.',
+  },
+  step5: {
+    head: 'Thank you!',
+    subHead:
+      'Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com.',
+  },
+};
---
+++ src/styles/constants.ts
@@ -0,0 +1,10 @@
+export const timer = {
+  default: '0.3s',
+  fast: '0.1s',
+  slow: '0.5s',
+};
+
+export const size = {
+  mobile: '600px',
+  desktop: '1100px',
+};
